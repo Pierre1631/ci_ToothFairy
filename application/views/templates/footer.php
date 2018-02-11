18a0676137b176ce877385abd6799d19f7e260db
Mary Colleen Nagera <mariakolinanagera@gmail.com>
2018-02-11 15:00:23 +0800

    Add files via upload

---
+++ application/views/templates/footer.php
@@ -0,0 +1,36 @@
+<!--- Footer -->
+
+<div id="footer">
+  <div class="container">
+  <div class="row">
+      <div class="col-xs-6 footer-para">
+        <ul>
+        <li> <a href="#!">About Us</a></li> 
+        <li> <a href="#!">Legal</a></li> 
+        <li> <a href="#!">FAQs</a></li> 
+      </ul>
+        <p>© The Creator All right reserved.</p>
+         
+       </div>
+      <div class="col-xs-6 text-right">
+        <a href=""><i class="fa fa-facebook"></i></a>
+        <a href=""><i class="fa fa-twitter"></i></a>
+        <a href=""><i class="fa fa-skype"></i></a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+<script type="text/javascript" src="assets/js/jquery.1.11.1.js"></script> 
+<script type="text/javascript" src="assets/js/bootstrap.js"></script> 
+<script type="text/javascript" src="assets/js/SmoothScroll.js"></script> 
+<script type="text/javascript" src="assets/js/jquery.prettyPhoto.js"></script> 
+<script type="text/javascript" src="assets/js/jquery.isotope.js"></script> 
+<script type="text/javascript" src="assets/js/jquery.parallax.js"></script> 
+<script type="text/javascript" src="assets/js/jqBootstrapValidation.js"></script> 
+<script type="text/javascript" src="assets/js/contact_me.js"></script> 
+<script type="text/javascript" src="assets/js/main.js"></script>
+</body>
+</html>
