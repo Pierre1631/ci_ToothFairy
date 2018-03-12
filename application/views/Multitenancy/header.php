e82d78ca74c699547e210f83d17227daf7e07c91
Mary Colleen Nagera <mariakolinanagera@gmail.com>
2018-03-12 22:16:28 +0800

    Add files via upload

---
+++ application/views/Multitenancy/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>The Creator</title>
+<meta name="description" content="">
+<meta name="author" content="">
+<style type="text/css">
+.error { color: red; font: 10px Tahoma; }
+</style>
+<!-- Bootstrap -->
+<link rel="stylesheet" type="text/css"  href="assets/assetsadmin/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="assets/assetsadmin/fonts/font-awesome/css/font-awesome.css">
+<link rel="stylesheet" type="text/css"  href="assets/assetsadmin/css/style.css">
+<link rel="stylesheet" type="text/css" href="assets/assetsadmin/css/prettyPhoto.css">
+<link rel="stylesheet" type="text/css" href="assets/assetsadmin/css/benchnine.css">
+<link rel="stylesheet" type="text/css" href="assets/assetsadmin/css/OpenSanscss">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800,600,300' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="assets/assetsadmin/js/modernizr.custom.js"></script>
+<link rel="stylesheet" href="assets/assetsadmin/css/style_login.css">
+</head>
+
+
+<!-- Header -->
+<div id="intro">
+  <div class="intro-body">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+          <h1><span class="rotate">TOOTH FAIRY MULTITENANCY</span></h1>
+          <p class="intro-text">Stand out with limitless opportunities</p>
+        </div>
+          <div class="container">
+            <a href="Login" class="btn">Get Started</a>
+          </div>
+          <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+          <script src='https://andwecode.com/wp-content/uploads/2015/10/jquery.leanModal.min_.js'></script>
+          <script  src="assets/assetsadmin/js/login.js"></script>
+      </div>
+    </div>
+  </div>
+</div>
