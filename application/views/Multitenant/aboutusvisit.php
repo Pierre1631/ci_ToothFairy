83269b64ad1b5e0266d700979230cbb712b3760e
Kenneth Domingo <35374391+kakeeennnn@users.noreply.github.com>
2018-03-19 20:29:42 +0800

    Add files via upload

--- application/views/Multitenant/aboutusvisit.php
+++ application/views/Multitenant/aboutusvisit.php
@@ -117,7 +117,7 @@
                 <div class="row">
                     <div class="col-md-8 ml-auto mr-auto text-center">
                         <h2 class="title">Who we are?</h2>
-                        <h4 class="description">According to the National Oceanic and Atmospheric Administration, Ted, Scambos, NSIDClead scentist, puts the potentially record low maximum sea ice extent tihs year down to low ice extent in the Pacific and a late drop in ice extent in the Barents Sea.</h4>
+                        <h4 class="description">Tooth Fairy is a Business to Business Website that provides our clients the most efficient and sustainable Web Subscription to its users.</h4>
                     </div>
                 </div>
                 <div class="separator separator-primary"></div>
@@ -126,26 +126,24 @@
                         <div class="col-md-6">
                             <div class="image-container image-left" style="background-image: url('assets/img/login.jpg')">
                                 <!-- First image on the left side -->
-                                <p class="blockquote blockquote-primary">"Over the span of the satellite record, Arctic sea ice has been declining significantly, while sea ice in the Antarctichas increased very slightly"
+                                <p class="blockquote blockquote-primary">"In the year 2018, ToothFairy created its own name into Multitenant Services for Dental clinics. By providing satisfaction to its clients and giving continued reliable service at its best."
                                     <br>
                                     <br>
-                                    <small>-NOAA</small>
+                                    <small>-ANC</small>
                                 </p>
                             </div>
                             <!-- Second image on the left side of the article -->
-                            <div class="image-container" style="background-image: url('assets/img/bg3.jpg')"></div>
+              <!--              <div class="image-container" style="background-image: url('assets/img/bg3.jpg')"></div> -->
                         </div>
                         <div class="col-md-5">
                             <!-- First image on the right side, above the article -->
-                            <div class="image-container image-right" style="background-image: url('assets/img/bg1.jpg')"></div>
-                            <h3>So what does the new record for the lowest level of winter ice actually mean</h3>
-                            <p>The Arctic Ocean freezes every winter and much of the sea-ice then thaws every summer, and that process will continue whatever happens with climate change. Even if the Arctic continues to be one of the fastest-warming regions of the world, it will always be plunged into bitterly cold polar dark every winter. And year-by-year, for all kinds of natural reasons, there’s huge variety of the state of the ice.
+                            <div class="image-container image-right" style="background-image: url('assets/img/bg1.jpg')"></div> 
+                            <h3>So why you need to choose Tooth Fairy?</h3>
+                            <p>Multitenant Websites usually provides Blogspots, Event Web-blogs, Food Catalogs and the likes. Here in Tooth Fairy, dental clinics that wants to have a seamless transactions and efficient services, YOU CAN HAVE IT NOW.
                             </p>
                             <p>
-                                For a start, it does not automatically follow that a record amount of ice will melt this summer. More important for determining the size of the annual thaw is the state of the weather as the midnight sun approaches and temperatures rise. But over the more than 30 years of satellite records, scientists have observed a clear pattern of decline, decade-by-decade.
-                            </p>
-                            <p>The Arctic Ocean freezes every winter and much of the sea-ice then thaws every summer, and that process will continue whatever happens with climate change. Even if the Arctic continues to be one of the fastest-warming regions of the world, it will always be plunged into bitterly cold polar dark every winter. And year-by-year, for all kinds of natural reasons, there’s huge variety of the state of the ice.
-                            </p>
+                                Dental Clinic Website is catered by Tooth Fairy. Where Dental clinics can have its own Clinic Website for their clients to have a fastest and reliable transaction anytime, anywhere.
+                            </p>            
                         </div>
                     </div>
                 </div>
@@ -153,7 +151,7 @@
         </div>
         <div class="section section-team text-center">
             <div class="container">
-                <h2 class="title">Here is our team</h2>
+                <h2 class="title">Here is our team</h2><br>
                 <div class="team">
                     <div class="row">
                         <div class="col-md-4">
