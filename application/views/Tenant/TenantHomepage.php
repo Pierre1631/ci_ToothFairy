c0e84a5e61305db019a65bd9ba532cacc0288c31
Kenneth Domingo <35374391+kakeeennnn@users.noreply.github.com>
2018-03-23 15:14:41 +0800

    Add files via upload

--- application/views/Tenant/TenantHomepage.php
+++ application/views/Tenant/TenantHomepage.php
@@ -8,18 +8,18 @@
 			<div class="item active">
 				<div class="container">
 					<div class="carousel-caption">
-						<h3>to edit this go to <span>line 10.</span></h3>
-						<p>application/views/TenantHomepage.php</p>
-						<h6>Lorem ipsum dolor sit amet  consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation.</h6>
+						<h3>Welcome to <span>Tooth Fairy Clinic</span></h3>
+						<p>Why Tooth Fairy?</p>
+						<h6>The services of a competent, experienced dentist is your best shot at improving your looks, for a brighter smile and more self-confidence. Finding the right dentist can help ensure that you are comfortable while undergoing the dental procedures because you have full trust on whoever is wielding the instruments.</h6>
 					</div>
 				</div>
 			</div>
 			<div class="item item1">
 				<div class="container">
 					<div class="carousel-caption">
-						<h3>to edit this go to <span>line 18.</span></h3>
-						<p>application/views/TenantHomepage.php</p>
-						<h6>Lorem ipsum dolor sit amet  consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation.</h6>
+						<h3>Love your <span>tooth</span></h3>
+						<p>Choose us</p>
+						<h6>When it comes to excellent dental services, trust only the dentists with extensive years of experience in the field. Dr. Donna H. Ferraren, the head dentist has 25 years experience. The team at Tooth Fairy Dental Clinic is known for their gentle yet highly competent hands to give you a brighter smile that you can be proud of!</h6>
 					</div>
 				</div>
 			</div>
@@ -36,35 +36,32 @@
 
 	<div class="agile-about">
 		<div class="agileits-about-btm">
-			<h3 class="heading-agileinfo">Welcome To Our Dental Clinic!<span>To edit this go to line 36 of views/TenantHomepage.php</span></h3>
+			<h3 class="heading-agileinfo">Welcome to our Dental Clinic<span>You too deserve to have an awesome smile!</span></h3>
 			<div class="container">
 				<div class="me-flex">
 					<div class="col-md-4 col-sm-4 ab1 agileits-about-grid1">
-						<h4 class="agileinfo-head">To edit this go to line 40</h4>
-						<p>To edit this go to line 41</p>
-						<h5>line 42</h5>
-						<h5>line 43</h5>
-						<h5>line 44</h5>
-						<h5>line 45</h5>
-						<h5>line 46</h5>
+						<h4 class="agileinfo-head">Services</h4>
+						<h5>Teeth Whitening</h5>
+						<h5>Dental implant</h5>
+						<h5>Dental crown</h5>
+						<h5>braces</h5>
+						<h5>veneers</h5>
 					</div>
 					<div class="col-md-4 col-sm-4 ab1 agileits-about-grid2">
-						<h4 class="agileinfo-head">To edit this go to line 49</h4>
-						<p>To edit this go to line 50</p>
-						<h5>line 51</h5>
-						<h5>line 52</h5>
-						<h5>line 53</h5>
-						<h5>line 54</h5>
-						<h5>line 55</h5>
+						<h4 class="agileinfo-head">Doctors</h4>
+						<h5>Dr. Jasmine Hernandez</h5>
+						<h5>Dr. Jasper Garcia</h5>
+						<h5>Dr. Gerard Peralta</h5>
+						<h5>Dr. Darren Sanchez</h5>
+						<h5>Dr. Mariel Aquino</h5>
 					</div>
 					<div class="col-md-4 col-sm-4 ab1 agileits-about-grid3">
-						<h4 class="agileinfo-head">To edit this go to line 58</h4>
-						<p>To edit this go to line 59</p>
-						<h5>line 60</h5>
-						<h5>line 61</h5>
-						<h5>line 62</h5>
-						<h5>line 63</h5>
-						<h5>line 64</h5>
+						<h4 class="agileinfo-head">Careers</h4>
+						<h5>Dentist Intern</h5>
+						<h5>Dentist</h5>
+						<h5>Secretary</h5>
+						<h5>Dental Technician</h5>
+						<h5>Dentist Assistant</h5>
 					</div>
 					<div class="clearfix"></div>
 				</div>
@@ -78,14 +75,13 @@
 				<div class="emergency_cases_top">
 					<div class="col-md-6 emergency_cases_left">
 						<h4>Opening Hours</h4>
-						<h6>line 76 - line 76&nbsp;<span class="eme">00:00 - 00:00</span></h6>
-						<h6>line 77 - line 77&nbsp;<span class="eme">00:00 - 00:00</span></h6>
-						<h6>line 78 - line 78&nbsp;<span class="eme">00:00 - 00:00</span></h6>
+						<h6>Mon - Fri&nbsp;<span class="eme">07:00 AM - 06:00 PM</span></h6>
+						<h6>Sat - Sun&nbsp;<span class="eme">08:00 AM - 03:00 PM</span></h6>
 					</div>
 					<div class="col-md-6 emergency_cases_right">
 						<h4>Emergency Cases</h4>
 						<h5><i class="glyphicon glyphicon-phone" aria-hidden="true"></i>09123456789</h5>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+						
 					</div>
 					<div class="clearfix"></div>
 				</div>
@@ -95,24 +91,24 @@
 
 	<div class="services">
 		<div class="container">
-			<h3 class="heading-agileinfo">Treatments<span>To edit this go to line 93 of views/TenantHomepage.php</span></h3>
+			<h3 class="heading-agileinfo">Services</h3>
 			<div class="services-top-grids">
 				<div class="col-md-4">
 					<div class="grid1">
-						<h4>Line 98</h4>
-						<p>Lorem ipsum dolor sit amet, Sed ut perspiciatis unde omnis iste natus error sit voluptatem </p>
+						<h4>Bridge</h4>
+						<p>A bridge is designed to provide a patient, who is missing a tooth or teeth in the same area of the mouth, the opportunity to have a false tooth made to fill the existing space. This is done by crowning the teeth on the either side of the space and attaching a false tooth/teeth to the crowns.</p>
 					</div>
 				</div>
 				<div class="col-md-4">
 					<div class="grid1">
-						<h4>Line 105</h4>
-						<p>Lorem ipsum dolor sit amet, Sed ut perspiciatis unde omnis iste natus error sit voluptatem </p>
+						<h4>Crowns</h4>
+						<p>A crown can be made from either gold or porcelain and is a cap that is specially designed to cover a tooth. Teeth are generally crowned for: the tooth may have fractured or a considerable amount of the tooth is filling leaving the tooth weak and susceptible to further breakage.</p>
 					</div>
 				</div>
 				<div class="col-md-4">
 					<div class="grid1">
-						<h4>Line 112</h4>
-						<p>Lorem ipsum dolor sit amet, Sed ut perspiciatis unde omnis iste natus error sit voluptatem </p>
+						<h4>Dentures</h4>
+						<p>The use of dentures is an easy way of replacing missing teeth, and is a lot more affordable than implant or bridge work. We are able to construct dentures for single tooth replacement, partial tooth loss, or replacement of all teeth. Dentures are constructed during a few visits to our practice. </p>
 					</div>
 				</div>
 				<div class="clearfix"></div>
@@ -120,20 +116,20 @@
 			<div class="services-bottom-grids">
 				<div class="col-md-4">
 					<div class="grid1">
-						<h4>Line 122</h4>
-						<p>Lorem ipsum dolor sit amet, Sed ut perspiciatis unde omnis iste natus error sit voluptatem </p>
+						<h4>Filling</h4>
+						<p>gradually construct a denture to suit every patient's individual needs Filling is a tooth retaining (conservative) treatment to restore the crown or the root destruction of the tooth (mostly caused by caries and trauma). Thanks to the continuous development of technology, nowadays the once popular amalgam fillings are mostly replaced by aesthetic fillings.</p>
 					</div>
 				</div>
 				<div class="col-md-4">
 					<div class="grid1">
-						<h4>Line 129</h4>
-						<p>Lorem ipsum dolor sit amet, Sed ut perspiciatis unde omnis iste natus error sit voluptatem </p>
+						<h4>Implants</h4>
+						<p>We have introduced a new technology to minimize discomfort and healing times when using and installing implants. The gum is not cut, meaning that there is almost no swelling or damage; therefore the patient will only experience minimal discomfort. In addition, the implants we use are high-tech German-made single units. </p>
 					</div>
 				</div>
 				<div class="col-md-4">
 					<div class="grid1">
-						<h4>Line 136</h4>
-						<p>Lorem ipsum dolor sit amet, Sed ut perspiciatis unde omnis iste natus error sit voluptatem </p>
+						<h4>Teeth Whitening</h4>
+						<p>Teeth whitening can be done in two different methods: (1) the home whitening system, prepared in our dental office and completed by you at home; and (2) an in-office whitening system which is completed in one visit. The in-office tooth whitening system uses a whitening solution that is stronger than the home whitening solution. </p>
 					</div>
 				</div>
 			</div>
